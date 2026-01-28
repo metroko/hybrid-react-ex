aa0505107cf51625db0d199def94a34d2432bf4d
metroko <ganondoor @outlook.com>
2026-01-28 14:27:15 +0200

    hooks

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,52 +1,37 @@
-import type {MediaItem} from 'hybrid-types/DBTypes';
-import {useState} from 'react';
+import type {MediaItem, MediaItemWithOwner, UserWithNoPassword} from 'hybrid-types/DBTypes';
+import {useEffect, useState} from 'react';
 import MediaRow from '../components/MediaRow';
 import SingleView from '../components/SingleView';
+import {fetchData} from '../utils/fetch-data';
 
 const Home = () => {
-  const mediaArray: MediaItem[] = [
-    {
-      media_id: 8,
-      user_id: 5,
-      filename: 'https://placehold.co/1200x800?text=Pic1&fontsize=120',
-      thumbnail: 'https://placehold.co/320x240?text=Thumb1&fontsize=20',
-      filesize: 170469,
-      media_type: 'image/jpeg',
-      title: 'Picture 1',
-      description: 'This is a placeholder picture.',
-      created_at: '2024-01-07T20:49:34.000Z',
-      screenshots: [],
-    },
-    {
-      media_id: 9,
-      user_id: 7,
-      filename: 'https://placehold.co/800x600?text=Pic2&fontsize=72',
-      thumbnail: 'https://placehold.co/320x240?text=Thumb2&fontsize=20',
-      filesize: 1002912,
-      media_type: 'image/jpeg',
-      title: 'Pic 2',
-      description: '',
-      created_at: '2024-01-07T21:32:27.000Z',
-      screenshots: [],
-    },
-    {
-      media_id: 17,
-      user_id: 2,
-      filename:
-        'https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_1MB.mp4',
-      thumbnail: 'https://placehold.co/320x240?text=Thumb3&fontsize=20',
-      filesize: 1236616,
-      media_type: 'video/mp4',
-      title: 'Bunny',
-      description: 'Butterflies fly around the bunny.',
-      created_at: '2024-01-07T20:48:13.000Z',
-      screenshots: [],
-    },
-  ];
-
+  const [mediaArray, setMediaArray] = useState<MediaItemWithOwner[]>([]);
   const [selectedItem, setSelectedItem] = useState<MediaItem | undefined>(
     undefined,
   );
+
+  useEffect(() => {
+    const getMedia = async () => {
+      const media = await fetchData<MediaItem[]>(
+        import.meta.env.VITE_MEDIA_API + '/media');
+
+      const mediaWithOwners = await Promise.all<MediaItemWithOwner>(
+        media.map(async (item) => {
+        const owner = await fetchData<UserWithNoPassword>(
+          import.meta.env.VITE_AUTH_API + '/users' + item.user_id
+        );
+        const mediaItemWithOwner: MediaItemWithOwner = {
+          ...item,
+          username: owner.username
+        };
+        return mediaItemWithOwner;
+      }),
+    );
+      setMediaArray(mediaWithOwners);
+      console.log(mediaWithOwners);
+    };
+    getMedia();
+  }, []);
 
   return (
     <>
@@ -62,6 +47,7 @@ const Home = () => {
             <th>Thumbnail</th>
             <th>Title</th>
             <th>Description</th>
+            <th>Owner</th>
             <th>Created</th>
             <th>Size</th>
             <th>Type</th>
