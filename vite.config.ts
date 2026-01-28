b6970e443afc0f4eeb8413b17b516ce6b0be93ca
metroko <ganondoor @outlook.com>
2026-01-28 14:42:25 +0200

    add base to vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/~rogerko/hybrid/react-routing/',
   test: {
     environment: 'jsdom',
     globals: true,
